61d66718fc55f04aee92dc3db7830abbc5838c21
Tony Oliverio <tony.oliverio1@gmail.com>
2017-07-08 03:39:00 -0700

    logic for weekly data

--- server/index.js
+++ server/index.js
@@ -10,40 +10,44 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 /*
   Set up mongo
-*/
+  */
 
-const mongoose = require('mongoose');
-mongoose.Promise = Promise;
-mongoose.connect('mongodb://sam:pass@ds145312.mlab.com:45312/heroku_gbvm74zg');
+  const mongoose = require('mongoose');
+  mongoose.Promise = Promise;
+  mongoose.connect('mongodb://sam:pass@ds145312.mlab.com:45312/heroku_gbvm74zg');
 
-var Schema = mongoose.Schema;
+  var Schema = mongoose.Schema;
 
 /**
   Datapoints
-*/
+  */
 
-const dataPointSchema = new Schema({
-  value: Number,
-  created_at: Date,
-});
-
-const DataPoint = mongoose.model('DataPoint', dataPointSchema);
-
-
-const roomSchema = new Schema({
-  data_points: [{
+  const dataPointSchema = new Schema({
     value: Number,
-    created_at: Date
-  }],
-  room_name: String
-});
+    created_at: Date,
+  });
 
-const Room = mongoose.model('Room', roomSchema);
+  const DataPoint = mongoose.model('DataPoint', dataPointSchema);
 
-app.post('/api/roomdata', (req, res) => {
-  res.set('Content-Type', 'application/json');
 
-  const roomName = req.body.room_name;
+  const roomSchema = new Schema({
+    data_points: [{
+      value: Number,
+      created_at: Date
+    }],
+    room_name: String,
+    daily_graph: {
+      data:[Number],
+      updated_at: Date
+    }
+  });
+
+  const Room = mongoose.model('Room', roomSchema);
+
+  app.post('/api/roomdata', (req, res) => {
+    res.set('Content-Type', 'application/json');
+
+    const roomName = req.body.room_name;
 
   // const dp = DataPoint({
   //   value: req.body.value,
@@ -55,15 +59,50 @@ app.post('/api/roomdata', (req, res) => {
     created_at: new Date()
   };
 
+
   Room.findOneAndUpdate({room_name: roomName}, {$push:{data_points: dp}}, {safe: true, upsert: true}, (err, model) => {
     if (err) {
       console.log("err");
       res.send({message:"error"});
     }
-    console.log("Success");
-    res.send({message: "Success"});
+    Room.findOne({'room_name':roomName}, (err, room) =>{
+      if(!room.daily_graph){
+        const graph = [0.0, 0.2, 0.5, 0.5, 0.6, 0.75, 0.4, 0.2, 0.0, 0.2, 0.5, 0.5, 0.6, 0.75, 0.4, 0.2, 0.0, 0.2, 0.5, 0.5, 0.6, 0.75, 0.4, 0.2];
+        room.daily_graph.data = graph;
+        room.daily_graph.last_update = new Date();
+        room.save((err) =>{
+          if(err){
+            console.log("Error updating new data");
+          }
+        })
+      }
+      else{
+        const current_date = new Date()
+        if(((current_date.getTime() - room.daily_graph.last_update.getTime())/1000.0)>3600){
+
+          const datapoints = room.datapoints;
+          const count = 0;
+          const total = 0;
+          for (var i =0; i < datapoints.length; i++){
+            if(datapoints[i].created_at > room.daily_graph.last_update){
+              total+= datapoints[i].value;
+              count++;
+            }
+          }
+          const average = count / float(total);
+          var new_data = room.daily_graph.slice(1);
+          new_data.add(average);
+          room.daily_graph.data = new_data;
+          room.daily_graph.last_update = current_date;
+          room.save((err)=>{
+            console.log("Error updating new data");
+          });
+        }
+      } 
+      console.log("Success");
+      res.send({message: "Success"});
+    });
   });
-});
 
 
 
@@ -123,10 +162,48 @@ app.get('/api/rooms', (req, res) => {
 //   })
 // })
 
+app.get('api/:room_name/dailygraph', (req, res)=>{
+  res.set('Content-Type', 'application/json');
+  const roomName = req.params.room_name;
+  const query = {
+    'room_name':roomName,
+  }
+  Room.findOne({'room_name':roomName}, (err, room) =>{
+    if(!room.daily_graph){
+      const graph = [0.0, 0.2, 0.5, 0.5, 0.6, 0.75, 0.4, 0.2];
+      room.daily_graph.data = graph;
+      room.daily_graph.last_update = new Date();
+      room.save((err) =>{
+        if(err){
+          console.log("Error updating new data");
+        }
+      })
+    }
+    else{
+      current_date = new Date()
+      if(((current_date.getTime() - last_update.getTime())/1000.0)>3600){
+        genNewDailyGraph()
+      }
+    }
+    const last_update = room.daily_graph.last_update;
+    const current_date = new Date()
+    if(!last_update){
+
+    } else if (((current_date.getTime() - last_update.getTime())/1000.0)>3600){ //3600 seconds in a hour
+
+    }
+  })
+});
+
+function genNewDailyGraph(data, cb){
+
+  cb()
+}
+
 
 /**
  End Datapoints
-*/
+ */
 
 /**
 
